334d078450eb7c5fbf71a9fbee6dbf161faac5ff
Nicholas Villarreal <nicholasvillarreal@gmail.com>
2019-01-02 17:02:17 -0700

    Improve overall experience and robustness of commands
        - Add test before starting so that we do not try to start multiple
        times
        - Improve error messages when things don't work right
        - Add version info to help output

--- index.js
+++ index.js
@@ -32,10 +32,12 @@ const pidFile = `${installPath}/sonos-web.pid`;
 function asyncExec(command) {
   return new Promise((resolve, reject) => {
     shell.exec(command, { silent: true, async: false }, (code, stdout, stderr) => {
-      const stream = fs.createWriteStream(installLogFile, { flags: 'a' });
-      stream.write(stdout);
-      stream.write(stderr);
-      stream.end();
+      if (shell.test('-f', installLogFile)) {
+        const stream = fs.createWriteStream(installLogFile, { flags: 'a' });
+        stream.write(stdout);
+        stream.write(stderr);
+        stream.end();
+      }
       if (code !== 0) {
         reject(code);
       } else {
@@ -53,8 +55,8 @@ function logError(description) {
   console.log(colors.bold.red('error ') + description);
 }
 
-function logInfo(description) {
-  console.log(colors.bold.magenta('info ') + description);
+function isRunning() {
+  return shell.test('-f', pidFile);
 }
 
 
@@ -67,16 +69,24 @@ function isInstalled() {
  */
 async function start() {
   const noPortErrorMessage = 'Could not find PORT environment variable';
+  const alreadyRunningMessage = `${colors.yellow('Sonos Web')} is already running`;
+  const noInstallationFoundMessage = `No installation found...run ${colors.cyan('sonos-web install')} to get started`;
   try {
-    spinner.start(`Starting ${colors.yellow('Sonos Web')}`);
-    if (!isInstalled()) throw new Error();
-    shell.cd(installPath);
+    if (!isInstalled()) throw new Error(noInstallationFoundMessage);
+    // this means that sonos-web is already running
+    // because forever deletes this file when it is stopped
+    if (isRunning()) {
+      throw new Error(alreadyRunningMessage);
+    }
 
+    shell.cd(installPath);
     const envFile = await fs.readFile('.env');
     const portEnv = envFile.toString().split('\n').find(env => env.includes('PORT='));
     if (!portEnv) {
       throw (new Error(noPortErrorMessage));
     }
+
+    spinner.start(`Starting ${colors.yellow('Sonos Web')}`);
 
     await asyncExec(`${foreverPath} --pidFile ${pidFile} -a -l ${foreverLogFile} -o ${logFile} -e ${logFile} start src/server.js`);
     spinner.succeed();
@@ -90,48 +100,53 @@ async function start() {
     logSuccess(`Open a web browser on this computer to ${colors.cyan(`http://localhost:${port}`)} to start!`);
     console.log(`You can access ${colors.yellow('Sonos Web')} network-wide by going to ${colors.cyan(sonosNetworkAddress)}`);
   } catch (err) {
-    spinner.fail();
-    if (err.message === noPortErrorMessage) {
-      logError(noPortErrorMessage);
-    } else {
-      logError(`no installation found...run ${colors.cyan('sonos-web install')} to get started`);
+    switch (err.message) {
+      case noPortErrorMessage:
+      case noInstallationFoundMessage:
+        logError(err.message);
+        break;
+      case alreadyRunningMessage:
+        console.log(err.message);
+        break;
+      default:
+        spinner.fail();
+        logError(err);
     }
-
     shell.exit(1);
   }
 }
 
 async function stop() {
-  try {
-    spinner.start(`Stopping ${colors.yellow('Sonos Web')}`);
-    if (!isInstalled()) throw new Error();
-    shell.cd(installPath);
-  } catch (err) {
-    spinner.fail();
-    logError('no installation found');
-    shell.exit(1);
+  if (!isRunning()) {
+    console.log(`${colors.yellow('Sonos Web')} isn't running`);
+    return;
   }
 
+  spinner.start(`Stopping ${colors.yellow('Sonos Web')}`);
+
   try {
+    shell.cd(installPath);
     await asyncExec(`${foreverPath} stop src/server.js`);
     spinner.succeed();
   } catch (err) {
     spinner.fail();
-    logInfo(`${colors.yellow('Sonos Web')} isn't running`);
+    logError(err);
   }
 }
 
 async function uninstall() {
-  console.log(`Uninstalling ${colors.yellow('Sonos Web')}`);
+  if (!shell.test('-d', installPath)) {
+    console.log(`${colors.yellow('Sonos Web')} isn't installed`);
+    return;
+  }
+  console.log(`Stopping ${colors.yellow('Sonos Web')} if it is running...`);
   // stop the server if it is running
   await stop();
-
   try {
-    spinner.start('removing installation');
-    // Remove server fiforeveles & directory
-    fs.removeSync(installPath);
+    spinner.start(`Uninstalling ${colors.yellow('Sonos Web')}`);
+    // Remove server files & directory
+    await fs.remove(installPath);
     spinner.succeed();
-    logSuccess(`${colors.yellow('Sonos Web')} uninstalled successfully`);
   } catch (err) {
     spinner.fail();
     logError('uninstall failed');
@@ -218,7 +233,7 @@ async function update() {
 
 program
   .version(version)
-  .description('CLI tool for installing Sonos Web'.cyan);
+  .description(`CLI tool (v${version}) for installing Sonos Web`.cyan);
 
 program
   .command('install')
